91f0eb66b748aea5c19829b95c93acef0f4b2df0
Eugen <Re4port@ukr.net>
2016-11-24 23:26:47 +0200

    change status even if value = 'true'

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -2,7 +2,21 @@ $(document).ready(function() {
 	var chackBoxTag = $(".checked");
 
     chackBoxTag.click(function(e) {
-      $(this).closest(".forchack").toggleClass('stroked');
+       	//console.log($(this).attr('value'));
+    	if ($(this).attr('value') == 'false') {
+    	  $(this).closest(".forchack").toggleClass('stroked');
+	    	  /*отправить Ajax, 
+	    	  сохранить в базу task.status как 'true', 
+	    	  изменить значение value='true', 
+	    	  "Задание ВЫПОЛНЕННО"*/
+    	}else{
+    		$(this).closest(".forchack").removeClass('stroked');
+	    	  /*изменить value='false', 
+	          task.status = 'false', 
+	    	  "Задание НЕ сделано"*/
+    		alert("Removed");
+    	 }
+      
     });
     
     $(".icon-move").click(function(){
@@ -11,16 +25,20 @@ $(document).ready(function() {
 
     $(function strokeCheckedTasks(){		//strocked//
     	chackBoxTag.each(function(i,elem) {
-    		//chackBoxTag.closest(".forchack").toggleClass('stroked');
-    		console.log(elem.getAttribute('value'));
-    		var valueOfCheckbox = elem.getAttribute('value');
+    	//chackBoxTag.closest(".forchack").toggleClass('stroked');
+    	//console.log(elem.getAttribute('value'));
+    	//var valueOfCheckbox = elem.getAttribute('value');
     		//alert(valueOfCheckbox);
-    	  if (valueOfCheckbox == 'true') {
-    	  	
+    	  if (elem.getAttribute('value') == 'true') {
     	  	$(this).closest(".forchack").toggleClass('stroked');
-    	  }
+          }
+
     	});
     	
+    });
+
+    $(function removeStrokeForTask(){ /*remove stroked style*/
+
     });
    
 });
