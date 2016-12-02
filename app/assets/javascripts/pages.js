eb072a162c534b45a0969ddec5c69ebe1c4818b3
evgeniy <Re4port@ukr.net>
2016-12-02 13:22:38 +0200

    fix problem with correct name and old_name when open more then one edit_project_name

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -2,11 +2,10 @@ $(document).ready(function() {
 	var chackBoxTag = $(".checked");
 
     chackBoxTag.click(function(e) {
-       // e.preventDefault();
         var taskId = $(this).attr('id');
         var thisChackBox = $(this);
         var task_object = thisChackBox.closest("tr.forchack").find("td.tasks");
-    //console.log($(this).attr('value'));
+
     	if ($(this).attr('value') == 'false') {
             /*отправить Ajax, 
               сохранить в базу task.status как 'true', 
@@ -17,16 +16,14 @@ $(document).ready(function() {
                       url: "/tasks/editTaskStatus/",
                       data: {id: taskId, value: true},
                       success: function(data) { 
-                      console.log(data);
-                       
-                    }
-                      /*dataType: "json"*/
+                        console.log(data);
+                      }
+
                     })
                       .done(function( msg ) {
                         task_object.toggleClass('stroked');
                         thisChackBox.val(true);
-                       // thisChackBox.attr("checked", true);
-                        /*alert( "Data Saved: " + msg );*/
+
                       })
                         .error(function (a) {
                           thisChackBox.val(false);
@@ -35,53 +32,36 @@ $(document).ready(function() {
                         });
           return true;
     	}else{
-              /*изменить value='false', 
-              task.status = 'false', 
-              "Задание НЕ сделано"*/
               $.ajax({
                       method: "POST",
                       url: "/tasks/editTaskStatus/",
                       data: {id: taskId, value: false},
                       success: function(data) { 
-                      console.log(data);
-                       
-                    }
-                      /*dataType: "json"*/
-                    })
-                      .done(function( msg ) {
-                        task_object.removeClass('stroked');
-                        thisChackBox.val(false);
-                      //  thisChackBox.attr("checked", false);
-                        /*alert( "Data Saved: " + msg );*/
-                      })
-                        .error(function (a) {
-                          thisChackBox.val(true);
-                          thisChackBox.prop("checked", true);
-                          alert('error');
-                        });
-    		//$(this).closest(".forchack").removeClass('stroked');
-        //$(this).val(false);
-    		//alert("Removed");
-    	 }
+                        console.log(data);
+                      }
+                      
+              })
+                .done(function( msg ) {
+                  task_object.removeClass('stroked');
+                  thisChackBox.val(false);
+                })
+                  .error(function (a) {
+                    thisChackBox.val(true);
+                    thisChackBox.prop("checked", true);
+                    alert('error');
+                  });
+
+    	}
       
     });
-    
-    //$(".icon-move").click(function(){
-    //$(this).closest(".forchack").sortable();
-    //});
+   
 
     function strokeCheckedTasks(e){		//strocked//
     	e.each(function(i,elem) {
-    	//chackBoxTag.closest(".forchack").toggleClass('stroked');
-    	//console.log(elem.getAttribute('value'));
-    	//var valueOfCheckbox = elem.getAttribute('value');
-    	//alert(valueOfCheckbox);
     	  if (elem.getAttribute('value') == 'true') {
-            //console.log($(this));
-            $(this).prop( "checked", true ); /*add checked to chack_box_tag*/
-    	  	$(this).closest(".forchack").find(".tasks").toggleClass('stroked');
-            
-          }
+          $(this).prop( "checked", true ); /*add checked to chack_box_tag*/
+    	  	$(this).closest(".forchack").find(".tasks").toggleClass('stroked');   
+        }
 
     	});
     };
@@ -90,29 +70,29 @@ $(document).ready(function() {
 
 
 
-var old_project_name = "";
-var project_id = null;
-var project_title = null;
-var edit_project_links = $(".glyphicon.glyphicon-pencil.edit_project_name");
-  $(".edit_project_name").click(function(e){  
 
-    project_id = $(this).prop('id');
-    project_title = $("#project_name_" + project_id);/*select by  project_name_id*/
-    old_project_name = project_title.text();   
-    project_title.html(" <div class='input-group edit_project_name'> <input class='form-control edit_project_name_field' placeholder='Start typing here to edit a project name...' type='text'> <span class='input-group-btn'> <input type='submit' value='Edit Task' class='btn btn-danger edit_project_name_send_ajax' data-disable-with='Edit Task'> </span> ");     
-    edit_project_links.not($(this)).unbind('click'); /*disable ckick on other 'edit_project_name'*/
+//var edit_project_links = $(".glyphicon.glyphicon-pencil.edit_project_name");
+  $(".edit_project_name").click(function(e){  /**/
+
+    var project_id = $(this).prop('id');
+    var project_title = $("#project_name_" + project_id);/*select by  project_name_id*/
+    var old_project_name = project_title.text();   
+    project_title.html(" <div class='input-group edit_project_name'> <input class='form-control edit_project_name_field' placeholder='Start typing here to edit a project name...' type='text'> <span class='input-group-btn' value='"+old_project_name+"'> <input type='submit' value='Edit Task' class='btn btn-danger edit_project_name_send_ajax' id='bt_"+project_id+"' data-disable-with='Edit Task'> </span> "); 
     
     });
 
-
+//var current_project_id = null;
   $(document).on("click", ".edit_project_name_send_ajax", function(e){              /* edit project name with Ajax */
-    var new_name_of_project = $(this).parents().find("input.form-control.edit_project_name_field").val(); 
-    console.log(new_name_of_project + project_id);
+    var new_name_of_project = $(this).parent().parent().find("input.form-control.edit_project_name_field").val();
+    var current_project_id = $(this).attr('id').replace('bt_', '');
+    current_project_title = $("#project_name_" + current_project_id);
+    var old_project_name_for_current_project = $(this).parent(".input-group-btn").attr('value');
+    //console.log('new_name:'+new_name_of_project + ', pr_id:'+project_id +', current_pr_id: '+ current_project_id);
     
               $.ajax({
                       method: "POST",
                       url: "/projects/editProjectName/",
-                      data: {id: project_id, name: new_name_of_project},
+                      data: {id: current_project_id, name: new_name_of_project},
                       success: function(data) { 
                       console.log(data);
                        
@@ -120,11 +100,10 @@ var edit_project_links = $(".glyphicon.glyphicon-pencil.edit_project_name");
                       
                     })
                       .done(function( msg ) {
-                        project_title.text(new_name_of_project);
-                        edit_project_links.on("click");
+                        current_project_title.text(new_name_of_project);
                       })
                         .error(function (a) {
-                          project_title.text(old_project_name);
+                          current_project_title.text(old_project_name_for_current_project);
                           alert('error');
                         });
 
