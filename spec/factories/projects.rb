3e6285dbd75fc81125b49b546f05896f36ad6445
Eugen <Re4port@ukr.net>
2017-01-09 22:48:25 +0200

    add factories with assotiations

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :project do
+  	association(:user)
   	name "Name of project"
   	user_id 7
   end
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :task do
+  	association(:project)
   	name "Name of task"
   	project_id 7
   end
